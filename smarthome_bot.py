420ecf296cbf5ab4a99aef72faf2d26d406152e1
newsun87 <newsun87@mail.sju.edu.tw>
2020-07-02 20:51:20 +0800

    init.

--- smarthome_bot.py
+++ smarthome_bot.py
@@ -33,7 +33,7 @@ apikey = 'CWB-A247B989-2950-4B3B-8665-1D92E72BC2AB'
 access_token = "dWhp1zz+Irv8ktCX06FWQFpF0BwSrzs5VSBUK/Fp7NLG0kBAVZe2VTwko8d0KO3ajTOw/jlwJPtpPYe+dVhN6G0eWwbdoLbECjMEbQQriKKk/imWqL8mA19YOiF9JaGwD9gmmpnEhLjwQvXek8FkDwdB04t89/1O/w1cDnyilFU="
 channel_secret = "b396a51f336580d711303f8adf09816f"
 
-#camera_url = 'unknown'  
+camera_url = 'unknown'  
 
 app = Flask(__name__)
 
@@ -1203,7 +1203,8 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("genurl", 0) 
     client.subscribe("homesecurity/ngrokurl", 0)       
 
-def on_message(client, userdata, msg):      
+def on_message(client, userdata, msg): 
+	global camera_url     
     print(msg.topic + " " + str(msg.payload))
     if msg.topic == 'homesecurity/ngrokurl':
      camera_url = str(msg.payload)
