95b7f6644fc5adf59e05fa71ae900ef76ef6ef98
newsun87 <newsun87@mail.sju.edu.tw>
2020-07-07 07:13:29 -0700

    init.

--- smarthome_bot.py
+++ smarthome_bot.py
@@ -341,9 +341,9 @@ def handle_postback_message(event):
        QuickReply_text_message = getQuickReply_pm25() # 取得 pm25 快速選單      
        line_bot_api.reply_message(event.reply_token, QuickReply_text_message)
        
-    elif postBack == 'AI_Image':
-       QuickReply_text_message = getQuickReply_AI_Image() # 取得 pm25 快速選單      
-       line_bot_api.reply_message(event.reply_token, QuickReply_text_message) 
+   *** elif postBack == 'AI_Image':
+		QuickReply_text_message = getQuickReply_AI_Image() # 取得 pm25 快速選單 
+		line_bot_api.reply_message(event.reply_token, QuickReply_text_message)*** 
 # ---------------------------------------------------------------            
        
     elif postBack == 'plugs':
@@ -1276,8 +1276,7 @@ def live_menu():
                 PostbackAction(
                     label = '圖片辨識', # 在按鈕模板上顯示的名稱
                     data = 'AI_Image'                    
-                )
-                
+                )                
             ]
          )
         )
