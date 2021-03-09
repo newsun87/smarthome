400bd3ee1f89979bc35fb4006b190d8964205f74
newsun87 <newsun87@mail.sju.edu.tw>
2021-03-09 14:17:05 +0800

    init.

--- smarthome_bot.py
+++ smarthome_bot.py
@@ -1212,7 +1212,9 @@ def get_weather_state(weather_info,cityname):
     elif "多雲時陰" in weather_info :
           imageurl = 'https://i.imgur.com/QI5NI4P.png'
     elif "多雲時陰短暫雨" in weather_info :
-          imageurl = 'https://i.imgur.com/KswxMPl.png'          
+          imageurl = 'https://i.imgur.com/KswxMPl.png'
+    elif "多雲短暫雨" in weather_info :
+          imageurl = 'https://i.imgur.com/gFwgxmA.png'          
     elif "晴時多雲" in weather_info:
           imageurl = 'https://i.imgur.com/M69byPq.png'	    	               
     elif "多雲" in weather_info:	
