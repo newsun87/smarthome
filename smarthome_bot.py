47fbcb2af5b4cd8cb1ade9c5bf1f734de444a6e5
newsun87 <newsun87@mail.sju.edu.tw>
2021-03-09 09:34:40 +0800

    init.

--- smarthome_bot.py
+++ smarthome_bot.py
@@ -1610,9 +1610,9 @@ def scheduler_task():
     scheduler = APScheduler()
     scheduler.init_app(app)
     #定時任務，每隔10s執行1次
-    scheduler.add_job(func=scheduled_job, trigger='interval', seconds=10,id='my_job_id' )
+    #scheduler.add_job(func=scheduled_job, trigger='interval', seconds=10,id='my_job_id' )
     #scheduler.add_job(scheduled_job, 'interval', seconds=10,id='my_job_id' )
-    #scheduler.add_job(func=scheduled_job, trigger='cron', hour='9-14',id='my_job_id' )
+    scheduler.add_job(func=scheduled_job, trigger='cron', seconds='*/10',id='my_job_id' )
     scheduler.start()
     
 def on_connect(client, userdata, flags, rc):  
