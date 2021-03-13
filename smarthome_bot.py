479b472a74031152ad3539545e02062ec43644b7
newsun87 <newsun87@mail.sju.edu.tw>
2021-03-13 12:45:19 +0800

    init.

--- smarthome_bot.py
+++ smarthome_bot.py
@@ -936,12 +936,8 @@ def getQuickReply_pm25():
           QuickReplyButton(
             action = MessageAction(label = "桃園市", text = "pm25~桃園市"),
             image_url = 'https://i.imgur.com/cSx1PLC.png'
-          ), 
-          
-          QuickReplyButton(
-            action = MessageAction(label = "桃園市", text = "pm25~桃園市"),
-            image_url = 'https://i.imgur.com/cSx1PLC.png'
           ),           
+          
           QuickReplyButton(
             action = MessageAction(label = "新竹市", text = "pm25~新竹市"),
             image_url = 'https://i.imgur.com/cSx1PLC.png'
@@ -951,7 +947,7 @@ def getQuickReply_pm25():
             image_url = 'https://i.imgur.com/cSx1PLC.png'
           ),  
            QuickReplyButton(
-            action = MessageAction(label = "臺中市", text = "pm25~台中市"),
+            action = MessageAction(label = "臺中市", text = "pm25~臺中市"),
             image_url = 'https://i.imgur.com/cSx1PLC.png'
           ), 
            QuickReplyButton(
@@ -973,15 +969,7 @@ def getQuickReply_pm25():
            QuickReplyButton(
             action = MessageAction(label = "屏東縣", text = "pm25~屏東縣"),
             image_url = 'https://i.imgur.com/cSx1PLC.png'
-          ),
-          QuickReplyButton(
-            action = MessageAction(label = "臺東縣", text = "pm25~臺東縣"),
-            image_url = 'https://i.imgur.com/cSx1PLC.png'
-          ),
-          QuickReplyButton(
-            action = MessageAction(label = "花蓮縣", text = "pm25~花蓮縣"),
-            image_url = 'https://i.imgur.com/cSx1PLC.png'
-          ),
+          ),          
           QuickReplyButton(
             action = MessageAction(label = "宜蘭縣", text = "pm25~宜蘭縣"),
             image_url = 'https://i.imgur.com/cSx1PLC.png'
