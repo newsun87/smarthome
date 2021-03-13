9304b4431703b0dfdbe746204763d21dcce1a39d
newsun87 <newsun87@mail.sju.edu.tw>
2021-03-13 17:36:53 +0800

    init.

--- smarthome_bot.py
+++ smarthome_bot.py
@@ -1248,7 +1248,9 @@ def get_pm25(cityname): #取得 PM2.5資訊
          % (len(data_list), cityname, count, round(PM25/count), round(AQI/count))
       print(AQI_info)
       if round(AQI/count) > 0 and round(AQI/count) < 50:
-        imageurl = 'https://i.imgur.com/4SJSYlp.png'        
+        imageurl = 'https://i.imgur.com/4SJSYlp.png'
+      elif round(AQI/count) > 50 and round(AQI/count) < 100:
+        imageurl = 'https://i.imgur.com/4SJSYlp.png'         
       message_all = [
          TextSendMessage(text="["+cityname + "] 空氣品質： " +  AQI_info),
          ImageSendMessage(original_content_url= imageurl, \
