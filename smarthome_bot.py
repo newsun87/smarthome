4d5b38a859099e58e5e5bce16aa5be9e7bf8f358
newsun87 <newsun87@mail.sju.edu.tw>
2021-03-13 16:36:51 +0800

    init.

--- smarthome_bot.py
+++ smarthome_bot.py
@@ -1241,7 +1241,7 @@ def get_pm25(cityname): #取得 PM2.5資訊
         PM25 = PM25 + int(item["PM2.5"])
         AQI = AQI + int(item["AQI"])
         count = count+1       
-      message = "全台灣共有%d個測站，在%s共有%d個測站, PM2.5平均值為%f", 空氣品質平均指標(AQI)為%d \
+      message = "全台灣共有%d個測站，在%s共有%d個測站, PM2.5平均值為%f, 空氣品質平均指標(AQI)為%d" \
          % (len(data_list), cityname, count, round(PM25/count), round(AQI/count))
       print(message)
       return message 
