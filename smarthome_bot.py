e66f9da556ebf8df3688ab67d9f60f3d915de9e5
newsun87 <newsun87@mail.sju.edu.tw>
2021-03-14 12:52:42 +0800

    init.

--- smarthome_bot.py
+++ smarthome_bot.py
@@ -382,6 +382,11 @@ def handle_message(event):
 	  line_bot_api.reply_message(event.reply_token, message)
 	  
 # -----遠端攝影機 quickreply 的指令操作--------------
+  elif event.message.text.startswith('cameraID'): 
+      split_array = event.message.text.split("~")
+      cameraid = split_array [1]
+      ref.child(base_users_userId + userId + '/camera').update({"camera_ID":cameraid})
+      line_bot_api.reply_message(event.reply_token, message)  
   elif event.message.text == 'register_camera':
       message = TextSendMessage(text = "註冊攝影機: " + host + '/camera_register')                 
       line_bot_api.reply_message(event.reply_token, message)  
