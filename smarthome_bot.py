dd591a2858c3799e5f9d7dcaff8acf204e571c5f
newsun87 <newsun87@mail.sju.edu.tw>
2021-03-16 09:38:41 +0800

    init.

--- smarthome_bot.py
+++ smarthome_bot.py
@@ -1667,7 +1667,7 @@ client.connect("broker.mqttdashboard.com", 1883)
 client.loop_start()
 
 if __name__ == "__main__":           
-  app.run(debug=True, host='0.0.0.0', port=5000)    
+  app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)    
 
     
     
